d198274706a29704632906ad16a60b970294ca0e
MisterFDF <defalco@laboratoriomister.it>
2024-03-15 16:25:17 +0100

    New version

--- scripts/switch.js
+++
@@ -1,7 +0,0 @@
-function switchVisibility(varName) {
-    var r = document.querySelector(':root')
-    var v = getComputedStyle(r)
-    if (v.getPropertyValue(varName) == 'hidden')
-        r.style.setProperty(varName, 'visible')
-    else r.style.setProperty(varName, 'hidden')
-}
